63a0f63d47cd399e6683a77de0a1a5e905e28887
KriaPia <kristoffer.berg8@gmail.com>
2023-12-03 15:03:50 +0100

    Restructured project and changed build system
    * Changed main CMakeLists.txt to use targets
    * Updated version of gtest
    * Added test rule in Cmake
    * Added rule to configure Cmake in Makefile

--- test/template/template_test.cpp
+++ test/template/template_test.cpp
@@ -5,9 +5,9 @@ extern "C" {
 
 namespace {
 
-TEST(TemplateTest, positive) { EXPECT_EQ(3, addition(1, 2)); }
+TEST(Template, positive) { EXPECT_EQ(3, addition(1, 2)); }
 
-TEST(TemplateTest, negative) { EXPECT_NE(4, addition(-3, 2)); }
+TEST(Template, negative) { EXPECT_NE(4, addition(-3, 2)); }
 
-TEST(OtherTest, is_true) { EXPECT_NE(1, 1); }
+TEST(Other, is_true) { EXPECT_NE(1, 1); }
 } // namespace
